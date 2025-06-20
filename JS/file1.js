cc6b65b0220b3c53fa6e64552cac2bd7ee0dace7
Premkumar981 <premkumarsuru@gmail.com>
2025-06-20 09:58:48 +0530

    updated

---
+++ JS/file1.js
@@ -0,0 +1,27 @@
+// Introduction to JavaScript
+console.log("Hello!")
+
+let Name = "Prem"
+let age = 21
+let promoted = true
+let score
+let city = null
+
+console.log(Name)
+console.log(typeof Name)
+console.log(score)
+console.log(typeof city)
+
+console.log(typeof age)
+let strAge = String(age)
+console.log(typeof strAge)
+
+let rating = "5"
+console.log(typeof rating)
+
+let strRating = Number(rating)
+console.log(typeof strRating)
+
+let flag = "true"
+let bFlag = Boolean(flag)
+console.log(typeof bFlag)
---
+++ JS/file2.js
@@ -0,0 +1,21 @@
+// Operators in JavaScript
+let a = 35;
+let b = 10;
+
+console.log(a+b)
+console.log(a%b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+
+let c = "5"
+console.log(c+b)
+console.log(c*b)
+console.log(c==b)
+console.log(c===b)
+console.log(c!==a)
+console.log(a>b)
+console.log(a<b)
+console.log(a>b && a>c)
+console.log(!a>b)
+console.log(a**b)
