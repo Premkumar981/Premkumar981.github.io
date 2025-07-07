183b6d161d1b67a047ad190ba0f90fde6b105655
Premkumar981 <premkumarsuru@gmail.com>
2025-07-07 09:29:06 +0530

    updated

--- nodejs/nodeapp/index12.js
+++ nodejs/nodeapp/index12.js
@@ -1,10 +1,11 @@
+// let name1 = process.argv[2];
 // let name1 = process.argv[2] || "Prem";
 // console.log("Hello " + name1 + "!");
 
 import express from 'express'
 const app=express()
 
-const PORT = process.argv[2] || "8080"
+const PORT = process.argv[2] || "8080";
 
 app.listen(PORT,()=>{
     console.log(`Server started on port ${PORT}`)
